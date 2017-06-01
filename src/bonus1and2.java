1366d12b7ad2038c915e82bcac95ef1d015f040d
Latilres <theten@student.chalmers.se>
2017-06-01 17:20:38 +0200

    Klar med 1 och 2

--- src/bonus1and2.java
+++ src/bonus1and2.java
@@ -1,28 +1,54 @@
 public class bonus1and2{
-  int[] sekValues = { 1, 5, 10, 20, 50, 100, 500, 1000 };
   // parameters
   // amount the amount to be changed
   // val the different values arranged in ascending order
   // n the number of different values
   public static int change( int amount, int[] val, int n ){
-    int i = 0;
-    while (i =< n) {
-      if (amount < val[i]) {
-        // ta bort val[i] och framåt
-      }
-      i++;
-    }
-    if (i == n) {
+
+    // Are both the amount and n still in allowed ranges?
+    if (amount < val[0] || n <= 0) {
       return 0;
     }
-    // ropar på sig själv med amount-val[val.length], om detta är större än 0
+    // Have we reached the smallest allowed number? (have we succeded)
+    else if (amount == val[0]) {
+      return 1;
+    }
+    // The first change will decrease the length of val and the other change will subtract the biggest number still used in val from the amount.
+    else
+      return change(amount, val, n-1) + change(amount - val[n-1], val, n);
   }
 
-  public void permutations(String str){
+  public static void permutations(String str){
+    permutations("", str, str.length());
+  }
 
+  private static void permutations(String prefix, String str, int n){
+    // Print if there are no more letters to add to prefix
+    if (n <= 0)
+      System.out.println(prefix);
+    else {
+      // Loops over all letters in str to add all permutations of it to prefix
+      for (int i = 0; i < n; i++)
+        //permutations("add char to end of prefix", "remove the char we added to prefix", "nr of letters left next recursion")
+        permutations(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1), n-1);
+    }
   }
 
   public static void main(String[] args) {
-    System.out.println(change(12,sekValues,sekValues.length));
+    int[] sekValues = { 1, 5, 10, 20, 50, 100, 500, 1000 };
+    System.out.println("Antal sätt att växla " + change(12, sekValues, sekValues.length));
+    permutations("abc");
   }
 }
+
+/*
+ T(n) = T(n-1) * O(n) = n!
+ T(1) = O(1)
+  Since the first letter can be put at n different locations that leave n-1 locations for the second letter and so on down to the last letter.
+
+Ex:
+ T(3) = T(2) * O(3) =
+        T(1) * O(2) * O(3) =
+        O(1) * O(2) * O(3) =
+        1 * 2 * 3 = 3!
+  */
